158fdd14fbe83b1b5b68cbf1a42795305d970fc1
Yulia Osnova <yulia.osnova@gmail.com>
2024-03-25 16:25:56 +0200

    add versel.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import MainProvider from "./components/Context/MainProvider.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter basename="/">
+    <BrowserRouter basename="/">
       <MainProvider>
         <App />
       </MainProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
